a0966786efaa6dcb3fdb3d02a7b9dc1e47041d0f
rushLovenshier <ruwandi.2001.18@gmail.com>
2025-05-21 01:09:10 +0530

    utils added and updated

--- utils/jwtHelper.js
+++ utils/jwtHelper.js
@@ -1,5 +1,6 @@
 const jwt = require("jsonwebtoken");
 const secret = process.env.JWT_SECRET;
 
-exports.sign = (payload) => jwt.sign(payload, secret, { expiresIn: "24h" });
+exports.signToken = (payload) =>
+  jwt.sign(payload, secret, { expiresIn: "24h" });
 exports.verify = (token) => jwt.verify(token, secret);
